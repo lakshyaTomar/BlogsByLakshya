1445c169b794a0d391370a94b03e26f078da7be6
Lakshya Tomar <lakshyatomar2002@gmail.com>
2025-03-27 19:55:57 +0530

    Final changes done

--- src/main/java/com/blogservice/BlogServiceApplication.java
+++ src/main/java/com/blogservice/BlogServiceApplication.java
@@ -3,7 +3,7 @@ package com.blogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Main entry point for the Blog Service application.
+//Main entry point for the Blog Service application
 
 @SpringBootApplication
 public class BlogServiceApplication {
